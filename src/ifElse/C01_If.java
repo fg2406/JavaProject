27f5212b32ccff2f65054ef2faa3abe71b853dbe
fg2406 <flzgltr11@gmail.com>
2022-02-26 15:13:20 +0100

    ifTekrar

---
+++ src/ifElse/C01_If.java
@@ -0,0 +1,29 @@
+package ifElse;
+
+public class C01_If {
+
+
+    public static void main(String[] args) {
+
+        int sayi=95;
+
+         if (sayi>100){   //java bu kismindaki kosula bakar
+
+             System.out.println("sayi 100 den kucuk");
+
+         }
+
+
+         if (sayi<50){
+
+             System.out.println("sayi 50 den kucuk");
+
+         }
+
+
+
+    }
+
+
+
+}
---
+++ src/ifElse/C02_IfKullanimi.java
@@ -0,0 +1,32 @@
+package ifElse;
+
+public class C02_IfKullanimi {
+
+
+    public static void main(String[] args) {
+
+       String diller="arapca,almanca,ingilizce,fransizca";
+
+       if (diller.contains("ingilizce")){
+           System.out.println("ingilizce biliyrsun");
+
+
+       }
+
+       if (diller.equals("almanca")){
+
+           System.out.println("dogru");
+
+       }
+
+
+
+
+
+
+
+
+    }
+
+
+}
